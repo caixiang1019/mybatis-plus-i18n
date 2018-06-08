cf461ae85f51e94f45f065c9f058de94513bcaaf
xiang.cai <47074774@qq.com>
2018-06-08 14:51:25 +0800

    2

--- src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
+++ src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
@@ -11,5 +11,5 @@ public class MybatisPlusI18nApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusI18nApplication.class, args);
-    }
+    }//232
 }
