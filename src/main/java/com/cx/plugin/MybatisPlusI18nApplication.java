2504755be9e9fddd5c418c3e44c134f7c9da4c06
xiang.cai <47074774@qq.com>
2018-06-08 14:29:36 +0800

    init

--- src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
+++ src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
@@ -8,10 +8,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class})
 public class MybatisPlusI18nApplication {
-//323
+
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusI18nApplication.class, args);
     }
-//1
-    //234
+
 }
