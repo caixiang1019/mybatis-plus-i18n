04dd0e363c28cfd51b7b020afea2b393f1f0a5ec
xiang.cai <47074774@qq.com>
2018-06-08 14:09:54 +0800

    modify

--- src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
+++ src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
@@ -12,5 +12,5 @@ public class MybatisPlusI18nApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusI18nApplication.class, args);
     }
-
+    //myTest modify
 }
