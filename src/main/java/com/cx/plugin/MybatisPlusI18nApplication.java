cfe7ec1329166e79085b8e49bb6584ff63309fe9
xiang.cai <47074774@qq.com>
2018-06-26 14:26:08 +0800

    fix

--- src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
+++ src/main/java/com/cx/plugin/MybatisPlusI18nApplication.java
@@ -8,7 +8,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class})
 public class MybatisPlusI18nApplication {
-
+//323
+    //22
+    //asdfa
+    //SDF
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusI18nApplication.class, args);
     }
